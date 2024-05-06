37ca0d187bac3713d4bee04ff2cffa297b864420
Valery Shcherbakov <val032606@gmail.com>
2024-05-06 18:27:43 +0700

    install SQLAlchemy

---
+++ main.py
@@ -0,0 +1,30 @@
+import uvicorn
+from fastapi import FastAPI
+
+from items_views import router as items_router
+from users.views import router as users_router
+
+
+app = FastAPI()
+app.include_router(items_router)
+app.include_router(users_router)
+
+
+@app.get("/")
+def hello_index():
+    return {"message": "Hello index"}
+
+
+@app.get("/hello/")
+def hello(name: str = "World"):
+    name = name.strip().title()
+    return {"message": f"Hello {name}!"}
+
+
+@app.get("/calc/add/")
+def add(a: int, b: int):
+    return {"a": a, "b": b, "result": a + b}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
---
+++ users/crud.py
@@ -0,0 +1,13 @@
+"""
+Create
+Read
+Update
+Delete
+"""
+
+from users.schemas import CreateUser
+
+
+def create_user(user_in: CreateUser) -> dict:
+    user = user_in.model_dump()
+    return {"success": True, "user": user}
