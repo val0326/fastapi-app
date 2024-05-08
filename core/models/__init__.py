83216825d04d92506a57ad90c449e501649d16ad
Valery Shcherbakov <val032606@gmail.com>
2024-05-08 20:01:57 +0700

    create profile

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,7 +1,16 @@
-__all__ = ("Base", "DatabaseHelper", "db_helper", "Product", "User", "Post")
+__all__ = (
+    "Base",
+    "DatabaseHelper",
+    "db_helper",
+    "Product",
+    "User",
+    "Post",
+    "Profile",
+)
 
 from .base import Base
 from .db_helper import DatabaseHelper, db_helper
 from .post import Post
 from .product import Product
+from .profile import Profile
 from .user import User
--- core/models/post.py
+++ core/models/post.py
@@ -1,24 +1,17 @@
-from typing import TYPE_CHECKING
-
-from sqlalchemy import ForeignKey, String, Text
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
+from .mixins import UserRelationMixin
 
 
-if TYPE_CHECKING:
-    from .user import User
+class Post(UserRelationMixin, Base):
 
+    _user_back_populates = "posts"
 
-class Post(Base):
     title: Mapped[str] = mapped_column(String(100), unique=False)
     body: Mapped[str] = mapped_column(
         Text,
         default="",
         server_default="",
     )
-
-    user_id: Mapped[int] = mapped_column(
-        ForeignKey("users.id"),
-    )
-    user: Mapped["User"] = relationship(back_populates="posts")
